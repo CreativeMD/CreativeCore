a404ed2a592cd0e867fefdab4d347ee8c0bc35d9
CreativeMD <MatthiasHauer@gmx.de>
2015-04-16 21:51:31 +0200

    Small additions and fixes.

--- src/main/java/com/creativemd/creativecore/common/packet/CreativeCorePacket.java
+++ src/main/java/com/creativemd/creativecore/common/packet/CreativeCorePacket.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.Map.Entry;
 
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.Vec3;
 import io.netty.buffer.ByteBuf;
 import cpw.mods.fml.common.network.simpleimpl.IMessage;
 import cpw.mods.fml.relauncher.Side;
@@ -47,4 +48,18 @@ public static final HashMap<String, Class<? extends CreativeCorePacket>> packets
 	
 	public abstract void executeServer(EntityPlayer player);
 	
+	public static void writeVec3(Vec3 vec, ByteBuf buf)
+	{
+		buf.writeDouble(vec.xCoord);
+		buf.writeDouble(vec.yCoord);
+		buf.writeDouble(vec.zCoord);
+	}
+	
+	public static Vec3 readVec3(ByteBuf buf)
+	{
+		double x = buf.readDouble();
+		double y = buf.readDouble();
+		double z = buf.readDouble();
+		return Vec3.createVectorHelper(x, y, z);
+	}
 }
