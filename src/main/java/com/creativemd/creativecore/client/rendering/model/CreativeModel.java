31de9413e5d24851ef63c5917ed98059a2e9ce72
CreativeMD <Creative-Multimedia-Design@gmx.de>
2016-07-27 13:38:10 +0200

    Added graph

---
+++ src/main/java/com/creativemd/creativecore/client/rendering/model/CreativeModel.java
@@ -0,0 +1,46 @@
+package com.creativemd.creativecore.client.rendering.model;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import com.google.common.base.Function;
+import com.google.common.collect.ImmutableSet;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.block.model.IBakedModel;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.client.renderer.vertex.VertexFormat;
+import net.minecraft.init.Blocks;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.model.IModel;
+import net.minecraftforge.client.model.ModelLoaderRegistry;
+import net.minecraftforge.common.model.IModelState;
+import net.minecraftforge.common.model.TRSRTransformation;
+
+public class CreativeModel implements IModel {
+	
+	public static CreativeModel instance = new CreativeModel();
+	
+	@Override
+	public Collection<ResourceLocation> getDependencies() {
+		return ImmutableSet.of();
+	}
+
+	@Override
+	public Collection<ResourceLocation> getTextures() {
+		return ImmutableSet.of();
+	}
+
+	@Override
+	public IBakedModel bake(IModelState state, VertexFormat format,
+			Function<ResourceLocation, TextureAtlasSprite> bakedTextureGetter) {
+		return Minecraft.getMinecraft().getBlockRendererDispatcher().getModelForState(Blocks.PLANKS.getDefaultState());
+		//return null;
+	}
+
+	@Override
+	public IModelState getDefaultState() {
+		return TRSRTransformation.identity();
+	}
+
+}
---
+++ src/main/java/com/creativemd/creativecore/utils/LinearGraph.java
@@ -0,0 +1,33 @@
+package com.creativemd.creativecore.utils;
+
+import java.util.HashMap;
+
+public class LinearGraph extends Graph {
+
+	public LinearGraph(HashMap<Float, Float> points) {
+		super(points);
+	}
+
+	@Override
+	public float getY(float x) {
+		Float pointBefore = getPreviousPointX(x);
+		Float pointAfter = getNextPointX(x);
+		if(pointBefore != null && pointAfter != null)
+		{
+			if(pointBefore == pointAfter)
+				return points.get(pointBefore);
+			float distance = pointAfter-pointBefore;
+			float relativePos = x-pointBefore;
+			float percentage = relativePos/distance;
+			float pointBeforeY = points.get(pointBefore);
+			float distanceY = points.get(pointAfter) - pointBeforeY;
+			return pointBeforeY+distanceY*percentage;
+		}
+		if(pointBefore != null)
+			return points.get(pointBefore);
+		if(pointAfter != null)
+			return points.get(pointAfter);
+		return 0;
+	}
+	
+}
