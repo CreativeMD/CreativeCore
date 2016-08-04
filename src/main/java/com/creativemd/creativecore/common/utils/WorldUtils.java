8cc363198d74141e072044750ab6660267a9b081
CreativeMD <Creative-Multimedia-Design@gmx.de>
2016-08-04 02:46:59 +0200

    Updated WorldUtils

--- src/main/java/com/creativemd/creativecore/common/utils/WorldUtils.java
+++ src/main/java/com/creativemd/creativecore/common/utils/WorldUtils.java
@@ -6,31 +6,32 @@ import net.minecraft.block.Block;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 public class WorldUtils {
 	
-	public static void dropItem(World world, ArrayList<ItemStack> stacks, int x, int y, int z)
+	public static void dropItem(World world, ArrayList<ItemStack> stacks, BlockPos pos)
 	{
 		for (int i = 0; i < stacks.size(); i++) {
-			dropItem(world, stacks.get(i), x, y, z);
+			dropItem(world, stacks.get(i), pos);
 		}
 	}
 	
-	public static void dropItem(World world, ItemStack stack, int x, int y, int z)
+	public static void dropItem(World world, ItemStack stack, BlockPos pos)
 	{
 		float f = 0.7F;
         double d0 = (double)(world.rand.nextFloat() * f) + (double)(1.0F - f) * 0.5D;
         double d1 = (double)(world.rand.nextFloat() * f) + (double)(1.0F - f) * 0.5D;
         double d2 = (double)(world.rand.nextFloat() * f) + (double)(1.0F - f) * 0.5D;
-        EntityItem entityitem = new EntityItem(world, (double)x + d0, (double)y + d1, (double)z + d2, stack);
+        EntityItem entityitem = new EntityItem(world, (double)pos.getX() + d0, (double)pos.getY() + d1, (double)pos.getZ() + d2, stack);
         entityitem.setPickupDelay(10);
 		world.spawnEntityInWorld(entityitem);
 	}
 	
 	public static void dropItem(EntityPlayer player, ItemStack stack)
 	{
-		dropItem(player.worldObj, stack, (int)player.posX, (int)player.posY, (int)player.posZ);
+		dropItem(player.worldObj, stack, player.getPosition());
 	}
 	
 	public static void dropItem(EntityPlayer player, ArrayList<ItemStack> stacks)
