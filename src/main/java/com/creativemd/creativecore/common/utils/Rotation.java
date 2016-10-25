a0e0efc73f6c7946c6a8efb9f358ff59ace6859f
CreativeMD <Creative-Multimedia-Design@gmx.de>
2016-10-25 14:24:45 +0200

    Added helper method for Rotations

--- src/main/java/com/creativemd/creativecore/common/utils/Rotation.java
+++ src/main/java/com/creativemd/creativecore/common/utils/Rotation.java
@@ -1,5 +1,7 @@
 package com.creativemd.creativecore.common.utils;
 
+import java.util.ArrayList;
+
 import net.minecraft.util.EnumFacing;
 
 public enum Rotation {
@@ -11,8 +13,45 @@ public enum Rotation {
 	DOWN,
 	DOWNX,
 	SOUTH,
-	NORTH,
-	UNKOWN;
+	NORTH;
+	//UNKOWN;
+	
+	public ArrayList<EnumFacing> getRotations()
+	{
+		ArrayList<EnumFacing> rotations = new ArrayList<>();
+		switch(this)
+		{
+		case EAST:
+			rotations.add(EnumFacing.EAST);
+			break;
+		case WEST:
+			rotations.add(EnumFacing.WEST);
+			break;
+		case UP:
+			rotations.add(EnumFacing.UP);
+			break;
+		case UPX:
+			rotations.add(EnumFacing.NORTH);
+			rotations.add(EnumFacing.UP);
+			rotations.add(EnumFacing.SOUTH);
+			break;
+		case DOWN:
+			rotations.add(EnumFacing.DOWN);
+			break;
+		case DOWNX:
+			rotations.add(EnumFacing.NORTH);
+			rotations.add(EnumFacing.DOWN);
+			rotations.add(EnumFacing.SOUTH);
+			break;
+		case SOUTH:
+			rotations.add(EnumFacing.SOUTH);
+			break;
+		case NORTH:
+			rotations.add(EnumFacing.NORTH);
+			break;
+		}
+		return rotations;
+	}
 	
 	public Rotation getOpposite()
 	{
@@ -34,9 +73,8 @@ public enum Rotation {
 			return DOWNX;
 		case WEST:
 			return EAST;
-		default:
-			return UNKOWN;
 		}
+		return EAST;
 	}
 	
 	public static Rotation getRotationByFacing(EnumFacing direction)
@@ -55,9 +93,8 @@ public enum Rotation {
 			return UP;
 		case WEST:
 			return WEST;
-		default:
-			return UNKOWN;
 		}
+		return EAST;
 	}
 	
 }
