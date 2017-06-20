a2bd26dc9e4755910118f20c39301d15dc1cbce2
CreativeMD <Creative-Multimedia-Design@gmx.de>
2017-06-20 16:14:44 +0200

    Fixed a couple of issues of splitted packets Added iterator for HashMapList

--- src/main/java/com/creativemd/creativecore/common/utils/HashMapList.java
+++ src/main/java/com/creativemd/creativecore/common/utils/HashMapList.java
@@ -1,21 +1,26 @@
 package com.creativemd.creativecore.common.utils;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map.Entry;
 import java.util.Set;
 
 public class HashMapList<K, V> {
-
+	
 	private HashMap<K, ArrayList<V>> keys;
 	
 	public HashMapList()
 	{
 		this.keys = new HashMap<>();
+	}
+	
+	public HashMapList(HashMapList<K, V> object)
+	{
+		this.keys = new HashMap<>(object.keys);
 	}
 	
 	public ArrayList<V> getValues(K key)
@@ -46,6 +51,24 @@ public class HashMapList<K, V> {
 	public Set<Entry<K, ArrayList<V>>> entrySet()
 	{
 		return keys.entrySet();
+	}
+	
+	public boolean contains(V value)
+	{
+		for (Iterator<ArrayList<V>> iterator = getValues().iterator(); iterator.hasNext();) {
+			ArrayList<V> type = iterator.next();
+			if(type.contains(value))
+				return true;
+		}
+		return false;
+	}
+	
+	public boolean contains(K key, V value)
+	{
+		ArrayList<V> list = getValues(key);
+		if(list != null)
+			return list.contains(value);
+		return false;
 	}
 	
 	public void add(K key, V[] values)
@@ -124,6 +147,58 @@ public class HashMapList<K, V> {
 
 	public boolean isEmpty() {
 		return sizeOfValues() > 0;
+	}
+
+	public V getFirst() {
+		if(size() > 0)
+		{
+			for (Iterator<ArrayList<V>> iterator = getValues().iterator(); iterator.hasNext();) {
+				ArrayList<V> list = iterator.next();
+				if(list.size() > 0)
+					return list.get(0);
+			}
+		}
+		return null;
+	}
+	
+	public Iterator<V> iterator()
+	{
+		return new Iterator<V>() {
+			
+			int index = 0;
+			
+			Iterator<ArrayList<V>> iterator = getValues().iterator();
+			
+			ArrayList<V> currentList;
+			
+			@Override
+			public boolean hasNext() {
+				while(currentList == null || currentList.size() <= index)
+				{
+					if(iterator.hasNext())
+					{
+						currentList = iterator.next();
+						index = 0;
+					}
+					else
+						return false;
+				}
+				
+				return true;
+			}
+
+			@Override
+			public V next() {
+				V value = currentList.get(index);
+				index++;
+				return value;
+			}
+			
+			@Override
+			public void remove() {
+				currentList.remove(index);
+			}
+		};
 	}
 	
 }
