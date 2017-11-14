11fdf6510a5e1642fdb5dd65378c96ffb2c452af
CreativeMD <Creative-Multimedia-Design@gmx.de>
2017-11-14 14:12:14 +0100

    Fixed WorldFake crash

--- src/main/java/com/creativemd/creativecore/common/world/ChunkProviderFake.java
+++ src/main/java/com/creativemd/creativecore/common/world/ChunkProviderFake.java
@@ -78,7 +78,7 @@ public class ChunkProviderFake implements IChunkProvider
         long i = ChunkPos.asLong(x, z);
         Chunk chunk = (Chunk)this.id2ChunkMap.get(i);
 
-        if (chunk != null)
+        if (worldObj.isRemote && chunk != null)
         {
             chunk.unloaded = false;
         }
