782333b24eaa952680fcf3890f2fd259ef665113
CreativeMD <Creative-Multimedia-Design@gmx.de>
2018-05-11 16:02:59 +0200

    Added blend function to ColorUtils

--- src/main/java/com/creativemd/creativecore/common/utils/ColorUtils.java
+++ src/main/java/com/creativemd/creativecore/common/utils/ColorUtils.java
@@ -131,4 +131,33 @@ public class ColorUtils {
         int b = color & 255;
 		return r == 255 && g == 255 && b == 255;
 	}
+	
+	public static int blend(int i1, int i2)
+	{
+		return blend(i1, i2, 0.5F);
+	}
+	
+	public static int blend(int i1, int i2, float ratio )
+	{
+	    if ( ratio > 1f ) ratio = 1f;
+	    else if ( ratio < 0f ) ratio = 0f;
+	    float iRatio = 1.0f - ratio;
+
+	    int a1 = (i1 >> 24 & 0xff);
+	    int r1 = ((i1 & 0xff0000) >> 16);
+	    int g1 = ((i1 & 0xff00) >> 8);
+	    int b1 = (i1 & 0xff);
+
+	    int a2 = (i2 >> 24 & 0xff);
+	    int r2 = ((i2 & 0xff0000) >> 16);
+	    int g2 = ((i2 & 0xff00) >> 8);
+	    int b2 = (i2 & 0xff);
+
+	    int a = (int)((a1 * iRatio) + (a2 * ratio));
+	    int r = (int)((r1 * iRatio) + (r2 * ratio));
+	    int g = (int)((g1 * iRatio) + (g2 * ratio));
+	    int b = (int)((b1 * iRatio) + (b2 * ratio));
+
+	    return a << 24 | r << 16 | g << 8 | b;
+	}
 }
