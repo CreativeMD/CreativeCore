20d66a866da55ef8400285692fcc1aef5431f15d
CreativeMD <Creative-Multimedia-Design@gmx.de>
2018-12-01 22:24:19 +0100

    Added drawRect method to RenderHelper

--- src/main/java/com/creativemd/creativecore/common/gui/GuiRenderHelper.java
+++ src/main/java/com/creativemd/creativecore/common/gui/GuiRenderHelper.java
@@ -112,6 +112,30 @@ public class GuiRenderHelper {
 		drawHorizontalGradientRect(left, top, right, bottom, (colour & ~mask) | 0xFF000000, colour | 0xFF000000 | mask);
 	}
 	
+	public void drawRect(double left, double top, double right, double bottom, int startColor) {
+		float f = (float) (startColor >> 24 & 255) / 255.0F;
+		float f1 = (float) (startColor >> 16 & 255) / 255.0F;
+		float f2 = (float) (startColor >> 8 & 255) / 255.0F;
+		float f3 = (float) (startColor & 255) / 255.0F;
+		GlStateManager.disableTexture2D();
+		GlStateManager.enableBlend();
+		GlStateManager.disableAlpha();
+		GlStateManager.tryBlendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
+		GlStateManager.shadeModel(7425);
+		Tessellator tessellator = Tessellator.getInstance();
+		BufferBuilder vertexbuffer = tessellator.getBuffer();
+		vertexbuffer.begin(7, DefaultVertexFormats.POSITION_COLOR);
+		vertexbuffer.pos((double) right, (double) top, 0).color(f1, f2, f3, f).endVertex();
+		vertexbuffer.pos((double) left, (double) top, 0).color(f1, f2, f3, f).endVertex();
+		vertexbuffer.pos((double) left, (double) bottom, 0).color(f1, f2, f3, f).endVertex();
+		vertexbuffer.pos((double) right, (double) bottom, 0).color(f1, f2, f3, f).endVertex();
+		tessellator.draw();
+		GlStateManager.shadeModel(7424);
+		GlStateManager.disableBlend();
+		GlStateManager.enableAlpha();
+		GlStateManager.enableTexture2D();
+	}
+	
 	public void drawHorizontalGradientRect(int left, int top, int right, int bottom, int startColor, int endColor) {
 		float f = (float) (startColor >> 24 & 255) / 255.0F;
 		float f1 = (float) (startColor >> 16 & 255) / 255.0F;
