6a27733aa6a82dee8b69ae5c8c2c9387fa80005c
CreativeMD <Creative-Multimedia-Design@gmx.de>
2019-01-06 16:26:58 +0100

    Added ChatFormatting for server and client side

---
+++ src/main/java/com/creativemd/creativecore/common/utils/mc/ChatFormatting.java
@@ -0,0 +1,64 @@
+package com.creativemd.creativecore.common.utils.mc;
+
+import java.util.Locale;
+
+public enum ChatFormatting {
+	BLACK('0'),
+	DARK_BLUE('1'),
+	DARK_GREEN('2'),
+	DARK_AQUA('3'),
+	DARK_RED('4'),
+	DARK_PURPLE('5'),
+	GOLD('6'),
+	GRAY('7'),
+	DARK_GRAY('8'),
+	BLUE('9'),
+	GREEN('a'),
+	AQUA('b'),
+	RED('c'),
+	LIGHT_PURPLE('d'),
+	YELLOW('e'),
+	WHITE('f'),
+	OBFUSCATED('k', true),
+	BOLD('l', true),
+	STRIKETHROUGH('m', true),
+	UNDERLINE('n', true),
+	ITALIC('o', true),
+	RESET('r');
+	
+	public static final String PREFIX_CODE = "§";
+	private final char code;
+	private final boolean isFormat;
+	private final String toString;
+	
+	private ChatFormatting(char code) {
+		this(code, false);
+	}
+	
+	private ChatFormatting(char code, boolean isFormat) {
+		this.code = code;
+		this.isFormat = isFormat;
+		
+		this.toString = PREFIX_CODE + code;
+	}
+	
+	public char getChar() {
+		return this.code;
+	}
+	
+	public boolean isFormat() {
+		return this.isFormat;
+	}
+	
+	public boolean isColor() {
+		return !this.isFormat && this != RESET;
+	}
+	
+	public String getName() {
+		return this.name().toLowerCase(Locale.ROOT);
+	}
+	
+	public String toString() {
+		return this.toString;
+	}
+}
