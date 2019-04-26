50749c5b0cba387a267278a55a01f198d502f077
CreativeMD <Creative-Multimedia-Design@gmx.de>
2019-04-26 15:47:01 +0200

    Added UUIDSupplier

---
+++ src/main/java/com/creativemd/creativecore/common/utils/type/UUIDSupplier.java
@@ -0,0 +1,28 @@
+package com.creativemd.creativecore.common.utils.type;
+
+import java.util.UUID;
+
+public class UUIDSupplier {
+	
+	private final UUID original;
+	public UUID uuid;
+	
+	public UUIDSupplier() {
+		this(UUID.randomUUID());
+	}
+	
+	public UUIDSupplier(UUID uuid) {
+		this.original = uuid;
+		this.uuid = uuid;
+	}
+	
+	public UUID original() {
+		return original;
+	}
+	
+	public UUID next() {
+		this.uuid = new UUID(uuid.getMostSignificantBits() + 1, uuid.getLeastSignificantBits());
+		return uuid;
+	}
+	
+}
