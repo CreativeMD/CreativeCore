3de566d1eac7fa8635987bcf3811f996a108556d
CreativeMD <Creative-Multimedia-Design@gmx.de>
2019-05-18 13:04:28 +0200

    Added IRenderChunkSupplier to CreativeWorld

---
+++ src/main/java/com/creativemd/creativecore/client/rendering/IRenderChunkSupplier.java
@@ -0,0 +1,10 @@
+package com.creativemd.creativecore.client.rendering;
+
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+public interface IRenderChunkSupplier {
+	
+	public Object getRenderChunk(World world, BlockPos pos);
+	
+}
