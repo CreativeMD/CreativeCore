850e377c4f5ee20c1ef9bfa8893470fc10dc2bb3
CreativeMD <Creative-Multimedia-Design@gmx.de>
2019-07-27 22:22:26 +0200

    Added the ability to use the mouse wheel to change the value of sliders (hold ctrl to change more)

--- src/main/java/com/creativemd/creativecore/common/gui/controls/gui/GuiSteppedSlider.java
+++ src/main/java/com/creativemd/creativecore/common/gui/controls/gui/GuiSteppedSlider.java
@@ -1,5 +1,7 @@
 package com.creativemd.creativecore.common.gui.controls.gui;
 
+import net.minecraft.client.gui.GuiScreen;
+
 public class GuiSteppedSlider extends GuiAnalogeSlider {
 	
 	public GuiSteppedSlider(String name, int x, int y, int width, int height, int value, int min, int max) {
@@ -17,15 +19,23 @@ public class GuiSteppedSlider extends GuiAnalogeSlider {
 	}
 	
 	@Override
+	public boolean mouseScrolled(int x, int y, int scrolled) {
+		setValue(value + (GuiScreen.isCtrlKeyDown() ? 10 : 1) * (scrolled > 0 ? 1 : -1));
+		return true;
+	}
+	
+	@Override
 	public void mouseMove(int posX, int posY, int button) {
 		super.mouseMove(posX, posY, button);
 		value = (int) value;
 	}
 	
+	@Override
 	public void setValue(float value) {
 		super.setValue((int) value);
 	}
 	
+	@Override
 	protected GuiTextfield createTextfield() {
 		return super.createTextfield().setNumbersIncludingNegativeOnly();
 	}
