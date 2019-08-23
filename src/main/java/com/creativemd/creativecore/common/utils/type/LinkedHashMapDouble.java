1d13b3d698b732cf24ab1f3323596e6f64c1bf61
CreativeMD <Creative-Multimedia-Design@gmx.de>
2019-08-23 11:43:59 +0200

    Added more primitive class types

---
+++ src/main/java/com/creativemd/creativecore/common/utils/type/LinkedHashMapDouble.java
@@ -0,0 +1,31 @@
+package com.creativemd.creativecore.common.utils.type;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class LinkedHashMapDouble<K> extends LinkedHashMap<K, Double> {
+	
+	public LinkedHashMapDouble() {
+		super();
+	}
+	
+	public LinkedHashMapDouble(Map<? extends K, ? extends Double> paramMap) {
+		super(paramMap);
+	}
+	
+	@Override
+	public Double put(K paramK, Double paramV) {
+		Double value = get(paramK);
+		if (value != null)
+			value += paramV;
+		return super.put(paramK, value);
+	}
+	
+	@Override
+	public void putAll(Map<? extends K, ? extends Double> paramMap) {
+		for (Entry<? extends K, ? extends Double> entry : paramMap.entrySet()) {
+			put(entry.getKey(), entry.getValue());
+		}
+	}
+}
