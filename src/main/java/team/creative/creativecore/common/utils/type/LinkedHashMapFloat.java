cd23836ec1af372df8e82c5166caf423e17ab38e
CreativeMD <Creative-Multimedia-Design@gmx.de>
2019-09-29 22:53:40 +0200

    Ported types from 1.12

---
+++ src/main/java/team/creative/creativecore/common/utils/type/LinkedHashMapFloat.java
@@ -0,0 +1,33 @@
+package team.creative.creativecore.common.utils.type;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class LinkedHashMapFloat<K> extends LinkedHashMap<K, Float> {
+	
+	public LinkedHashMapFloat() {
+		super();
+	}
+	
+	public LinkedHashMapFloat(Map<? extends K, ? extends Float> paramMap) {
+		super(paramMap);
+	}
+	
+	@Override
+	public Float put(K paramK, Float paramV) {
+		Float value = get(paramK);
+		if (value != null)
+			value += paramV;
+		else
+			value = paramV;
+		return super.put(paramK, value);
+	}
+	
+	@Override
+	public void putAll(Map<? extends K, ? extends Float> paramMap) {
+		for (Entry<? extends K, ? extends Float> entry : paramMap.entrySet()) {
+			put(entry.getKey(), entry.getValue());
+		}
+	}
+}
