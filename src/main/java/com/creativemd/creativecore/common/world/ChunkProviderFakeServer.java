0bc35c6848bff7566799950386d7b012106c83f6
CreativeMD <Creative-Multimedia-Design@gmx.de>
2020-05-25 13:11:28 +0200

    Fixed SpongeForge compatibility

--- src/main/java/com/creativemd/creativecore/common/world/ChunkProviderFakeServer.java
+++ src/main/java/com/creativemd/creativecore/common/world/ChunkProviderFakeServer.java
@@ -2,11 +2,15 @@ package com.creativemd.creativecore.common.world;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.spongepowered.common.bridge.world.chunk.ChunkProviderBridge;
 
+import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.chunk.storage.IChunkLoader;
 import net.minecraft.world.gen.IChunkGenerator;
+import net.minecraftforge.fml.common.Optional.Interface;
 
-public class ChunkProviderFakeServer extends ChunkProviderFake {
+@Interface(modid = "spongeforge", iface = "org.spongepowered.common.bridge.world.chunk.ChunkProviderBridge")
+public class ChunkProviderFakeServer extends ChunkProviderFake implements ChunkProviderBridge {
 	
 	private static final Logger LOGGER = LogManager.getLogger();
 	
@@ -22,5 +26,15 @@ public class ChunkProviderFakeServer extends ChunkProviderFake {
 	@Override
 	public boolean canSave() {
 		return false;
+	}
+	
+	@Override
+	public void bridge$setMaxChunkUnloads(int maxUnloads) {
+		
+	}
+	
+	@Override
+	public Chunk bridge$getLoadedChunkWithoutMarkingActive(int x, int z) {
+		return getLoadedChunk(x, z);
 	}
 }
