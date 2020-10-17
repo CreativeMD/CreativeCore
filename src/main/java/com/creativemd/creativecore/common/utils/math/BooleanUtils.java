6afd66bdb57653f77b14416826516a7f97ac3d70
CreativeMD <Creative-Multimedia-Design@gmx.de>
2020-10-17 20:25:53 +0200

    Removed inside from RangedBitSet

--- src/main/java/com/creativemd/creativecore/common/utils/math/BooleanUtils.java
+++ src/main/java/com/creativemd/creativecore/common/utils/math/BooleanUtils.java
@@ -12,22 +12,20 @@ public class BooleanUtils {
 	
 	public static boolean oneTrue(boolean... array) {
 		boolean wasTrue = false;
-		for (int i = 0; i < array.length; i++) {
+		for (int i = 0; i < array.length; i++)
 			if (array[i])
 				if (wasTrue)
 					return false;
 				else
 					wasTrue = true;
-		}
 		return wasTrue;
 	}
 	
 	public static int countTrue(boolean... array) {
 		int count = 0;
-		for (int i = 0; i < array.length; i++) {
+		for (int i = 0; i < array.length; i++)
 			if (array[i])
 				count++;
-		}
 		return count;
 	}
 	
