82b70ba2e0a08bbb11676aa9d7b79ea03d3889b9
CreativeMD <Creative-Multimedia-Design@gmx.de>
2021-01-29 14:15:05 +0100

    Added color palette to color picker

--- src/main/java/com/creativemd/creativecore/CreativeCoreConfig.java
+++ src/main/java/com/creativemd/creativecore/CreativeCoreConfig.java
@@ -1,7 +1,12 @@
 package com.creativemd.creativecore;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import com.creativemd.creativecore.common.config.api.CreativeConfig;
 import com.creativemd.creativecore.common.config.sync.ConfigSynchronization;
+import com.creativemd.creativecore.common.utils.mc.ColorUtils;
 
 public class CreativeCoreConfig {
     
@@ -10,5 +15,27 @@ public class CreativeCoreConfig {
     
     @CreativeConfig(name = "use-optifine-compat", type = ConfigSynchronization.CLIENT)
     public boolean useOptifineCompat = true;
+    
+    @CreativeConfig(type = ConfigSynchronization.CLIENT)
+    public List<ColorPalette> palette = Arrays.asList(new ColorPalette("basic", ColorUtils.WHITE, ColorUtils.BLACK, ColorUtils.RED, ColorUtils.GREEN, ColorUtils.BLUE));
+    
+    public static class ColorPalette {
+        
+        @CreativeConfig
+        public String name = "";
+        
+        @CreativeConfig
+        public List<Integer> colors = new ArrayList<>();
+        
+        public ColorPalette() {
+            
+        }
+        
+        public ColorPalette(String name, int... colors) {
+            this.name = name;
+            for (int i = 0; i < colors.length; i++)
+                this.colors.add(colors[i]);
+        }
+    }
     
 }
--- src/main/java/com/creativemd/creativecore/common/gui/controls/gui/GuiColorPicker.java
+++ src/main/java/com/creativemd/creativecore/common/gui/controls/gui/GuiColorPicker.java
@@ -1,18 +1,33 @@
 package com.creativemd.creativecore.common.gui.controls.gui;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.lwjgl.util.Color;
 
+import com.creativemd.creativecore.CreativeCore;
+import com.creativemd.creativecore.CreativeCoreConfig.ColorPalette;
+import com.creativemd.creativecore.common.config.holder.CreativeConfigRegistry;
+import com.creativemd.creativecore.common.config.sync.ConfigurationChangePacket;
+import com.creativemd.creativecore.common.gui.client.style.ColoredDisplayStyle;
+import com.creativemd.creativecore.common.gui.client.style.DisplayStyle;
 import com.creativemd.creativecore.common.gui.client.style.Style;
 import com.creativemd.creativecore.common.gui.container.GuiParent;
 import com.creativemd.creativecore.common.gui.event.gui.GuiControlChangedEvent;
+import com.creativemd.creativecore.common.packet.PacketHandler;
+import com.creativemd.creativecore.common.utils.mc.ColorUtils;
 import com.creativemd.creativecore.common.utils.mc.ColorUtils.ColorPart;
+
+import net.minecraft.init.SoundEvents;
+import net.minecraftforge.fml.relauncher.Side;
 
 public class GuiColorPicker extends GuiParent {
     
+    public GuiColorPickerPalette palette;
     public Color color;
     
     public GuiColorPicker(String name, int x, int y, Color color, boolean hasAlpha, int alphaMin) {
-        super(name, x, y, 140, hasAlpha ? 40 : 30);
+        super(name, x, y, 129, hasAlpha ? 40 : 30);
         marginWidth = 0;
         this.color = color;
         setStyle(Style.emptyStyle);
@@ -114,7 +129,16 @@ public class GuiColorPicker extends GuiParent {
             addControl(alpha.setStyle(defaultStyle));
         }
         addControl(new GuiColorPlate("plate", 107, 2, 20, 20, color).setStyle(defaultStyle));
-        
+        addControl(new GuiButton("more", 105, 28) {
+            
+            @Override
+            public void onClicked(int x, int y, int button) {
+                if (palette != null)
+                    closePalette();
+                else
+                    openPalette();
+            }
+        });
     }
     
     public void setColor(Color color) {
@@ -127,7 +151,185 @@ public class GuiColorPicker extends GuiParent {
     }
     
     public void onColorChanged() {
+        if (palette != null)
+            palette.onChanged();
         raiseEvent(new GuiControlChangedEvent(this));
+    }
+    
+    public void openPalette() {
+        palette = new GuiColorPickerPalette(name + "palette", this, posX, posY + height, width - getContentOffset() * 2, 100);
+        getGui().controls.add(palette);
+        
+        palette.parent = getGui();
+        palette.moveControlToTop();
+        palette.onOpened();
+        getGui().refreshControls();
+        palette.rotation = rotation;
+        palette.posX = getPixelOffsetX() - getGui().getPixelOffsetX() - getContentOffset();
+        palette.posY = getPixelOffsetY() - getGui().getPixelOffsetY() - getContentOffset() + height;
+        
+        if (palette.posY + palette.height > getParent().height && this.posY >= palette.height)
+            palette.posY -= this.height + palette.height;
+    }
+    
+    public void closePalette() {
+        if (palette != null) {
+            palette.savePalette();
+            getGui().controls.remove(palette);
+            removeListener(palette);
+            palette = null;
+        }
+    }
+    
+    private static final DisplayStyle SELECTED = new ColoredDisplayStyle(ColorUtils.YELLOW);
+    
+    public static class GuiColorPickerPalette extends GuiParent {
+        
+        public final GuiColorPicker picker;
+        public GuiColorPickerPaletteEntry selected;
+        public GuiButton add;
+        
+        public List<GuiColorPickerPaletteEntry> entries;
+        
+        public GuiColorPickerPalette(String name, GuiColorPicker picker, int x, int y, int width, int height) {
+            super(name, x, y, width, height);
+            this.picker = picker;
+            List<String> lines = new ArrayList<>();
+            CreativeCore.configHandler.loadClientFields();
+            for (ColorPalette palette : CreativeCore.config.palette)
+                lines.add(palette.name);
+            addControl(new GuiComboBox("palette", 0, 0, width - getContentOffset() * 2, lines));
+            this.add = new GuiButton("+", 0, 0, 10, 10) {
+                
+                @Override
+                public void onClicked(int x, int y, int button) {
+                    GuiColorPickerPaletteEntry entry = new GuiColorPickerPaletteEntry(GuiColorPickerPalette.this, "" + entries.size(), 0, 0, 10, 10, new Color(picker.color));
+                    entries.add(entry);
+                    addControl(entry);
+                    refreshEntries();
+                }
+            };
+            addControl(add);
+            addListener(this);
+            loadPalette();
+        }
+        
+        public void comboChanged(GuiControlChangedEvent event) {
+            if (event.source.is("palette"))
+                loadPalette();
+        }
+        
+        @Override
+        public void onClosed() {
+            super.onClosed();
+            savePalette();
+        }
+        
+        public void loadPalette() {
+            GuiComboBox box = (GuiComboBox) get("palette");
+            removeControls("palette", "+");
+            entries = new ArrayList<>();
+            if (box.index != -1) {
+                ColorPalette palette = CreativeCore.config.palette.get(box.index);
+                for (int i = 0; i < palette.colors.size(); i++) {
+                    GuiColorPickerPaletteEntry entry = new GuiColorPickerPaletteEntry(this, "" + i, 0, 0, 10, 10, ColorUtils.IntToRGBA(palette.colors.get(i)));
+                    entries.add(entry);
+                    addControl(entry);
+                }
+                
+            }
+            refreshEntries();
+        }
+        
+        public void savePalette() {
+            GuiComboBox box = (GuiComboBox) get("palette");
+            
+            if (box.index != -1) {
+                ColorPalette palette = CreativeCore.config.palette.get(box.index);
+                palette.colors = new ArrayList<>();
+                for (GuiColorPickerPaletteEntry entry : entries)
+                    palette.colors.add(ColorUtils.RGBAToInt(entry.getColor()));
+                String[] path = new String[] { "creativecore", "palette" };
+                if (CreativeCore.configHandler.isSynchronizedWithServer("creativecore.palette"))
+                    PacketHandler.sendPacketToServer(new ConfigurationChangePacket(path, CreativeConfigRegistry.ROOT.followPath(path).save(true, false, Side.SERVER)));
+                else
+                    CreativeCore.configHandler.save(Side.CLIENT);
+            }
+        }
+        
+        public void refreshEntries() {
+            int offsetY = 21;
+            int size = 18;
+            int perRow = (width - 5) / size;
+            for (int i = 0; i < entries.size(); i++) {
+                int row = i / perRow;
+                int col = i % perRow;
+                GuiColorPickerPaletteEntry entry = entries.get(i);
+                entry.posX = col * size;
+                entry.posY = offsetY + row * size;
+            }
+            int row = entries.size() / perRow;
+            int col = entries.size() % perRow;
+            add.posX = col * size;
+            add.posY = offsetY + row * size;
+        }
+        
+        @Override
+        public void onLoseFocus() {
+            if (!picker.isMouseOver() && !isMouseOver())
+                picker.closePalette();
+        }
+        
+        public void onChanged() {
+            if (selected != null)
+                selected.setColor(new Color(picker.color));
+        }
+        
+        @Override
+        public boolean canOverlap() {
+            return true;
+        }
+        
+    }
+    
+    public static class GuiColorPickerPaletteEntry extends GuiColorPlate {
+        
+        public final GuiColorPickerPalette palette;
+        
+        public GuiColorPickerPaletteEntry(GuiColorPickerPalette palette, String name, int x, int y, int width, int height, Color color) {
+            super(name, x, y, width, height, color);
+            this.palette = palette;
+        }
+        
+        @Override
+        public DisplayStyle getBorderDisplay(DisplayStyle display) {
+            if (palette.selected == this)
+                return SELECTED;
+            return super.getBorderDisplay(display);
+        }
+        
+        @Override
+        public boolean mousePressed(int x, int y, int button) {
+            if (button == 1) {
+                playSound(SoundEvents.UI_BUTTON_CLICK);
+                palette.entries.remove(this);
+                palette.removeControl(this);
+                if (palette.selected == this)
+                    palette.selected = null;
+                palette.refreshEntries();
+                return true;
+            }
+            palette.selected = this;
+            palette.picker.setColor(this.getColor());
+            playSound(SoundEvents.UI_BUTTON_CLICK);
+            return true;
+        }
+        
+        @Override
+        public void onLoseFocus() {
+            if (palette.selected == this)
+                palette.selected = null;
+        }
     }
     
 }
