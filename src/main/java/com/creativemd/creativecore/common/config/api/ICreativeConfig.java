b861cf6bfd6c46fbca759799a2ed602c3ddace89
CreativeMD <Creative-Multimedia-Design@gmx.de>
2021-08-09 11:07:36 +0200

    Added side aware configured method

--- src/main/java/com/creativemd/creativecore/common/config/api/ICreativeConfig.java
+++ src/main/java/com/creativemd/creativecore/common/config/api/ICreativeConfig.java
@@ -1,7 +1,13 @@
 package com.creativemd.creativecore.common.config.api;
 
+import net.minecraftforge.fml.relauncher.Side;
+
 public interface ICreativeConfig {
     
     public void configured();
+    
+    public default void configured(Side side) {
+        configured();
+    }
     
 }
