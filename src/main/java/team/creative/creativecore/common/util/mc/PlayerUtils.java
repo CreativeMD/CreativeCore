8200289865ebac85f4666741de065b8f3cd09d42
CreativeMD <Creative-Multimedia-Design@gmx.de>
2021-08-17 11:24:12 +0200

    Readded PlayerUtils

---
+++ src/main/java/team/creative/creativecore/common/util/mc/PlayerUtils.java
@@ -0,0 +1,22 @@
+package team.creative.creativecore.common.util.mc;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.GameType;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+public class PlayerUtils {
+    
+    @OnlyIn(Dist.CLIENT)
+    private static GameType getGameTypeClient(Player player) {
+        return Minecraft.getInstance().gameMode.getPlayerMode();
+    }
+    
+    public static GameType getGameType(Player player) {
+        if (player instanceof ServerPlayer)
+            return ((ServerPlayer) player).gameMode.getGameModeForPlayer();
+        return getGameTypeClient(player);
+    }
+}
