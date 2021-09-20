6f783447d9d10efb1f0e4042ba1a68c64b569033
CreativeMD <Creative-Multimedia-Design@gmx.de>
2021-09-20 11:28:49 +0200

    Fixed LayerPacket not working on client screens (not server bound)

--- src/main/java/team/creative/creativecore/common/gui/sync/LayerPacket.java
+++ src/main/java/team/creative/creativecore/common/gui/sync/LayerPacket.java
@@ -1,5 +1,6 @@
 package team.creative.creativecore.common.gui.sync;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
 import team.creative.creativecore.common.gui.integration.IGuiIntegratedParent;
@@ -15,6 +16,8 @@ public abstract class LayerPacket extends CreativePacket {
     public void executeClient(Player player) {
         if (player.containerMenu instanceof IGuiIntegratedParent)
             execute(player, (IGuiIntegratedParent) player.containerMenu);
+        else if (Minecraft.getInstance().screen instanceof IGuiIntegratedParent)
+            execute(player, (IGuiIntegratedParent) Minecraft.getInstance().screen);
     }
     
     @Override
