0597468aff4803ce35e23db9b373cb4b04dbd626
CreativeMD <Creative-Multimedia-Design@gmx.de>
2021-09-22 11:00:47 +0200

    Added LanguageUtils

---
+++ src/main/java/team/creative/creativecore/common/util/mc/LanguageUtils.java
@@ -0,0 +1,11 @@
+package team.creative.creativecore.common.util.mc;
+
+import net.minecraft.client.resources.language.I18n;
+
+public class LanguageUtils {
+    
+    public static String translate(String name) {
+        return I18n.get(name);
+    }
+    
+}
