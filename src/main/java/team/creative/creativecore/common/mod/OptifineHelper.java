cd4ef88fe3dbdadb555e6a8890ff6cfcecbb6cfc
CreativeMD <Creative-Multimedia-Design@gmx.de>
2022-02-24 16:00:11 +0100

    Added more methods to OptifineHelper

--- src/main/java/team/creative/creativecore/common/mod/OptifineHelper.java
+++ src/main/java/team/creative/creativecore/common/mod/OptifineHelper.java
@@ -3,9 +3,12 @@ package team.creative.creativecore.common.mod;
 import java.util.List;
 import java.util.Random;
 
+import com.mojang.blaze3d.vertex.BufferBuilder;
+
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.block.model.BakedQuad;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.state.BlockState;
@@ -19,6 +22,10 @@ public class OptifineHelper {
     
     public static boolean isEmissive(TextureAtlasSprite sprite) {
         return false;
+    }
+    
+    public static BakedModel getRenderModel(BakedModel model, LevelAccessor level, BlockState state, BlockPos pos) {
+        return model;
     }
     
     public static List<BakedQuad> getBakedQuad(List<BakedQuad> quads, LevelAccessor level, BlockState state, Facing facing, BlockPos pos, RenderType layer, Random rand) {
@@ -44,5 +51,11 @@ public class OptifineHelper {
     public static boolean isAntialiasing() {
         return false;
     }
+    
+    public static void pushBuffer(BlockState state, BlockPos pos, LevelAccessor level, BufferBuilder builder) {}
+    
+    public static void popBuffer(BufferBuilder builder) {}
+    
+    public static void calcNormalChunkLayer(BufferBuilder builder) {}
     
 }
