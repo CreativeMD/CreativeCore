f03a16fdb521d97ef500c3ca63af18028c17ffc3
CreativeMD <Creative-Multimedia-Design@gmx.de>
2022-03-14 16:08:05 +0100

    Added Side

---
+++ src/main/java/team/creative/creativecore/loader/Side.java
@@ -0,0 +1,32 @@
+package team.creative.creativecore.loader;
+
+public enum Side {
+    
+    CLIENT {
+        @Override
+        public boolean isClient() {
+            return true;
+        }
+        
+        @Override
+        public boolean isServer() {
+            return false;
+        }
+    },
+    SERVER {
+        @Override
+        public boolean isClient() {
+            return false;
+        }
+        
+        @Override
+        public boolean isServer() {
+            return true;
+        }
+    };
+    
+    public abstract boolean isClient();
+    
+    public abstract boolean isServer();
+    
+}
