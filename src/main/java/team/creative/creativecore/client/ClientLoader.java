b3a2aa353968e5be5f1606b472c36ff73848112d
CreativeMD <Creative-Multimedia-Design@gmx.de>
2022-04-17 22:37:13 +0200

    Fixed command registration for clients. See comment in ClientLoader.java for  required changes to other mods.

--- src/main/java/team/creative/creativecore/client/ClientLoader.java
+++ src/main/java/team/creative/creativecore/client/ClientLoader.java
@@ -2,12 +2,10 @@ package team.creative.creativecore.client;
 
 import com.mojang.brigadier.CommandDispatcher;
 
-import net.minecraft.commands.CommandSourceStack;
-
 public interface ClientLoader {
     
     public void onInitializeClient();
     
-    public default void registerClientCommands(CommandDispatcher<CommandSourceStack> dispatcher) {}
+    default <T> void registerClientCommands(CommandDispatcher<T> dispatcher) {}
     
 }
