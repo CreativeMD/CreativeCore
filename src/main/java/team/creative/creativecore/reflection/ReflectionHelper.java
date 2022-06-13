1cda48f1a749a08cbface659e6f060a9d2b0e639
CreativeMD <Creative-Multimedia-Design@gmx.de>
2022-06-13 13:46:18 +0200

    Added findMethod to ReflectionHelper

--- src/main/java/team/creative/creativecore/reflection/ReflectionHelper.java
+++ src/main/java/team/creative/creativecore/reflection/ReflectionHelper.java
@@ -1,6 +1,9 @@
 package team.creative.creativecore.reflection;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+import net.minecraftforge.fml.util.ObfuscationReflectionHelper.UnableToFindMethodException;
 
 public class ReflectionHelper {
     
@@ -27,8 +30,18 @@ public class ReflectionHelper {
             Field f = clazz.getDeclaredField(offical);
             f.setAccessible(true);
             return f;
-        } catch (Exception e2) {
-            throw new RuntimeException("Unable to locate field " + clazz.getSimpleName() + "." + offical, e2);
+        } catch (Exception e) {
+            throw new RuntimeException("Unable to locate field " + clazz.getSimpleName() + "." + offical, e);
+        }
+    }
+    
+    public static Method findMethod(Class<?> clazz, String methodName, Class<?>... parameterTypes) {
+        try {
+            Method m = clazz.getDeclaredMethod(methodName, parameterTypes);
+            m.setAccessible(true);
+            return m;
+        } catch (Exception e) {
+            throw new UnableToFindMethodException(e);
         }
     }
 }
