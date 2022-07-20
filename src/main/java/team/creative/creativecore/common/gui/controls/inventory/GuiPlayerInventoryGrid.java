cb92789fb4b6ac072dcbf8abaa3b90c2fa277965
CreativeMD <Creative-Multimedia-Design@gmx.de>
2022-07-20 15:58:24 +0200

    Fixed GuiPlayerInventoryGrid not reacting to inventory updates

--- src/main/java/team/creative/creativecore/common/gui/controls/inventory/GuiPlayerInventoryGrid.java
+++ src/main/java/team/creative/creativecore/common/gui/controls/inventory/GuiPlayerInventoryGrid.java
@@ -4,9 +4,21 @@ import net.minecraft.world.entity.player.Player;
 
 public class GuiPlayerInventoryGrid extends GuiInventoryGrid {
     
+    private int timesChanged;
+    
     public GuiPlayerInventoryGrid(Player player) {
         super("player", player.getInventory(), 9, 4);
+        this.timesChanged = player.getInventory().getTimesChanged();
         setExpandableX();
         this.reverse = true;
+    }
+    
+    @Override
+    public void tick() {
+        if (timesChanged != getPlayer().getInventory().getTimesChanged()) {
+            setChanged();
+            timesChanged = getPlayer().getInventory().getTimesChanged();
+        }
+        super.tick();
     }
 }
