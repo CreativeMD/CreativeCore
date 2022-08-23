d03c6edb98cd59af7a979d7ed83196d99a796301
CreativeMD <Creative-Multimedia-Design@gmx.de>
2022-08-23 19:43:18 +0200

    Small tweak to GuiParent get method

--- src/main/java/team/creative/creativecore/common/gui/GuiParent.java
+++ src/main/java/team/creative/creativecore/common/gui/GuiParent.java
@@ -125,11 +125,13 @@ public class GuiParent extends GuiControl implements IGuiParent, Iterable<GuiChi
         return null;
     }
     
-    public GuiControl get(String name) {
+    public <T extends GuiControl> T get(String name) {
+        if (name.isBlank())
+            return (T) this;
         GuiControl result = get(name, controls);
         if (result != null)
-            return result;
-        return get(name, hoverControls);
+            return (T) result;
+        return (T) get(name, hoverControls);
     }
     
     public <T extends GuiControl> T get(String name, Class<T> clazz) {
