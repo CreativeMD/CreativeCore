8bb9a99a54039f0f66984a4400930d9428649071
CreativeMD <Creative-Multimedia-Design@gmx.de>
2022-09-30 12:13:23 +0200

    Added FilterIterator

---
+++ src/main/java/team/creative/creativecore/common/util/type/itr/FilterIterator.java
@@ -0,0 +1,54 @@
+package team.creative.creativecore.common.util.type.itr;
+
+import java.util.Iterator;
+import java.util.function.Predicate;
+
+public class FilterIterator<T> implements Iterator<T> {
+    
+    private final Predicate predicate;
+    private final Iterator itr;
+    private T next;
+    
+    public FilterIterator(Iterable iterable, Class clazz) {
+        this(iterable.iterator(), clazz);
+    }
+    
+    public FilterIterator(Iterator iterable, Class clazz) {
+        this(iterable, x -> clazz.isInstance(x));
+    }
+    
+    public <E> FilterIterator(Iterable<E> iterable, Predicate<E> predicate) {
+        this(iterable.iterator(), predicate);
+    }
+    
+    public <E> FilterIterator(Iterator<E> iterator, Predicate<E> predicate) {
+        this.itr = iterator;
+        this.predicate = predicate;
+        findNext();
+    }
+    
+    private void findNext() {
+        while (next != null && itr.hasNext()) {
+            Object object = itr.next();
+            if (predicate.test(object)) {
+                next = (T) object;
+                return;
+            }
+        }
+        next = null;
+    }
+    
+    @Override
+    public boolean hasNext() {
+        return next != null;
+    }
+    
+    @Override
+    public T next() {
+        T result = next;
+        next = null;
+        findNext();
+        return result;
+    }
+    
+}
