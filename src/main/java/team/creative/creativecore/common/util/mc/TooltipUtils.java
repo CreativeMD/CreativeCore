5207332cb9b99168bd54228e628c2e42b48ba73f
CreativeMD <Creative-Multimedia-Design@gmx.de>
2023-02-02 15:37:17 +0100

    Changed TooltipUtils

--- src/main/java/team/creative/creativecore/common/util/mc/TooltipUtils.java
+++ src/main/java/team/creative/creativecore/common/util/mc/TooltipUtils.java
@@ -1,11 +1,15 @@
 package team.creative.creativecore.common.util.mc;
 
+import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.util.Locale;
 
 import net.minecraft.ChatFormatting;
 
 public class TooltipUtils {
+    
+    private static final NumberFormat NUMBER_FORMAT = NumberFormat.getNumberInstance(Locale.US);
+    private static final DecimalFormat DECIMAL_FORMAT = new DecimalFormat("0.##");
     
     public static String printColor(int color) {
         int r = ColorUtils.red(color);
@@ -15,9 +19,15 @@ public class TooltipUtils {
         return "" + ChatFormatting.RED + r + " " + ChatFormatting.GREEN + g + " " + ChatFormatting.BLUE + b + (a < 255 ? " " + ChatFormatting.WHITE + a : "");
     }
     
-    private static NumberFormat format = NumberFormat.getNumberInstance(Locale.US);
+    public static String print(int value) {
+        return NUMBER_FORMAT.format(value);
+    }
     
-    public static String printNumber(int value) {
-        return format.format(value);
+    public static String print(double value) {
+        return DECIMAL_FORMAT.format(value);
+    }
+    
+    public static String print(float value) {
+        return DECIMAL_FORMAT.format(value);
     }
 }
