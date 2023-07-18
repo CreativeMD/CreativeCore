fd373d640842ccee747ebeb3f4b0736f4af4d6c2
CreativeMD <Creative-Multimedia-Design@gmx.de>
2023-07-18 14:03:44 +0200

    Added printRoman to TooltipUtils

--- src/main/java/team/creative/creativecore/common/util/mc/TooltipUtils.java
+++ src/main/java/team/creative/creativecore/common/util/mc/TooltipUtils.java
@@ -30,4 +30,63 @@ public class TooltipUtils {
     public static String print(float value) {
         return DECIMAL_FORMAT.format(value);
     }
+    
+    public static String printRoman(int input) {
+        if (input < 1 || input > 3999)
+            return "" + input;
+        String s = "";
+        while (input >= 1000) {
+            s += "M";
+            input -= 1000;
+        }
+        while (input >= 900) {
+            s += "CM";
+            input -= 900;
+        }
+        while (input >= 500) {
+            s += "D";
+            input -= 500;
+        }
+        while (input >= 400) {
+            s += "CD";
+            input -= 400;
+        }
+        while (input >= 100) {
+            s += "C";
+            input -= 100;
+        }
+        while (input >= 90) {
+            s += "XC";
+            input -= 90;
+        }
+        while (input >= 50) {
+            s += "L";
+            input -= 50;
+        }
+        while (input >= 40) {
+            s += "XL";
+            input -= 40;
+        }
+        while (input >= 10) {
+            s += "X";
+            input -= 10;
+        }
+        while (input >= 9) {
+            s += "IX";
+            input -= 9;
+        }
+        while (input >= 5) {
+            s += "V";
+            input -= 5;
+        }
+        while (input >= 4) {
+            s += "IV";
+            input -= 4;
+        }
+        while (input >= 1) {
+            s += "I";
+            input -= 1;
+        }
+        return s;
+    }
 }
