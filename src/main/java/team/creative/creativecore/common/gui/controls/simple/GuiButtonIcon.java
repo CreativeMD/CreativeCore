98866aef2ea694b682fe86e0f19a8d5417b8e1ea
SrRapero720 <SrRapero720@hotmail.com>
2023-11-27 00:56:59 -0600

    Renamed GuiIconButton to GuiButtonIcon for consistency

--- src/main/java/team/creative/creativecore/common/gui/controls/simple/GuiButtonIcon.java
+++ src/main/java/team/creative/creativecore/common/gui/controls/simple/GuiButtonIcon.java
@@ -13,29 +13,29 @@ import team.creative.creativecore.client.render.GuiRenderHelper;
 import team.creative.creativecore.common.gui.GuiChildControl;
 import team.creative.creativecore.common.gui.GuiControl;
 import team.creative.creativecore.common.gui.style.ControlFormatting;
-import team.creative.creativecore.common.gui.style.GuiIcon;
+import team.creative.creativecore.common.gui.style.Icons;
 import team.creative.creativecore.common.util.math.geo.Rect;
 import team.creative.creativecore.common.util.type.Color;
 
-public class GuiIconButton extends GuiControl {
+public class GuiButtonIcon extends GuiControl {
     
     protected Consumer<Integer> pressed;
-    protected GuiIcon icon;
+    protected Icons icon;
     protected Color color = Color.WHITE;
     
-    public GuiIconButton(String name, GuiIcon icon, Consumer<Integer> pressed) {
+    public GuiButtonIcon(String name, Icons icon, Consumer<Integer> pressed) {
         super(name, 12, 12);
         this.icon = icon;
         this.pressed = pressed;
     }
     
-    public GuiIconButton(String name, int width, int height, GuiIcon icon, Consumer<Integer> pressed) {
+    public GuiButtonIcon(String name, int width, int height, Icons icon, Consumer<Integer> pressed) {
         super(name, width, height);
         this.icon = icon;
         this.pressed = pressed;
     }
     
-    public void setIcon(GuiIcon icon) {
+    public void setIcon(Icons icon) {
         this.icon = icon;
     }
     
@@ -49,10 +49,14 @@ public class GuiIconButton extends GuiControl {
     public void tick() {}
     
     @Override
-    public void flowX(int width, int preferred) {}
+    public GuiButtonIcon flowX(int width, int preferred) {
+        return this;
+    }
     
     @Override
-    public void flowY(int height, int preferred) {}
+    public GuiButtonIcon flowY(int height, int preferred) {
+        return this;
+    }
     
     @Override
     protected int preferredWidth() {
