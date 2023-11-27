871e6ac9230ab5c430e0ee68089893d0c9ab1993
SrRapero720 <SrRapero720@hotmail.com>
2023-11-27 01:05:52 -0600

    added px ValueDisplay

--- src/main/java/team/creative/creativecore/common/gui/ValueDisplay.java
+++ src/main/java/team/creative/creativecore/common/gui/ValueDisplay.java
@@ -2,6 +2,7 @@ package team.creative.creativecore.common.gui;
 
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TranslatableComponent;
+import team.creative.creativecore.common.util.time.TimeMath;
 
 public interface ValueDisplay {
     Component BLOCK_TEXT = new TranslatableComponent("minecraft.block");
@@ -10,6 +11,7 @@ public interface ValueDisplay {
     ValueDisplay PERCENT = (v, max) -> (int) ((v / max) * 100.0d) + "%";
     ValueDisplay ANGLE = (v, max) -> Math.round(v) + "°";
     ValueDisplay BLOCKS = (v, max) -> Math.round(v) + " " + (v > 1 ? BLOCKS_TEXT.getString() : BLOCK_TEXT.getString());
+    ValueDisplay PIXELS = (v, max) -> Math.round(v) + "px";
 
     String get(double value, double max);
 }
---
+++ src/main/java/team/creative/creativecore/common/util/time/TimeMath.java
@@ -0,0 +1,17 @@
+package team.creative.creativecore.common.util.time;
+
+import java.text.SimpleDateFormat;
+import java.util.concurrent.TimeUnit;
+
+public class TimeMath {
+    public static String timestamp(long time) {
+        if (time < 3600000) {
+            long min = TimeUnit.MILLISECONDS.toMinutes(time);
+            long sec = TimeUnit.MILLISECONDS.toSeconds(time) - TimeUnit.MINUTES.toSeconds(min);
+            return String.format("%02d:%02d", min, sec);
+        } else {
+            SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+            return sdf.format(time);
+        }
+    }
+}
