a2381add42f81d7d646fcabc1024a0bc83801892
CreativeMD <Creative-Multimedia-Design@gmx.de>
2024-01-16 15:04:03 +0100

    Added option to mob effect configuration to hide particles

--- src/main/java/team/creative/creativecore/common/config/premade/MobEffectConfig.java
+++ src/main/java/team/creative/creativecore/common/config/premade/MobEffectConfig.java
@@ -14,21 +14,24 @@ public class MobEffectConfig {
     public int amplifier;
     @CreativeConfig
     public int duration;
+    @CreativeConfig
+    public boolean hideParticles;
     
-    public MobEffectConfig(Registry<MobEffect> registry, ResourceLocation effect, int amplifier, int duration) {
+    public MobEffectConfig(Registry<MobEffect> registry, ResourceLocation effect, int amplifier, int duration, boolean hideParticles) {
         this.effect = new RegistryObjectConfig<MobEffect>(registry, effect);
         this.amplifier = amplifier;
         this.duration = duration;
+        this.hideParticles = hideParticles;
     }
     
     public MobEffectInstance create() {
-        return new MobEffectInstance(effect.value, duration, amplifier);
+        return new MobEffectInstance(effect.value, duration, amplifier, false, hideParticles);
     }
     
     @Override
     public boolean equals(Object obj) {
         if (obj instanceof MobEffectConfig mob)
-            return mob.effect.equals(effect) && mob.amplifier == amplifier && mob.duration == duration;
+            return mob.effect.equals(effect) && mob.amplifier == amplifier && mob.duration == duration && mob.hideParticles == hideParticles;
         return super.equals(obj);
     }
     
