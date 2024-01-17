1c4f43555c879d13269c8ac6e515e089bf963de7
CreativeMD <Creative-Multimedia-Design@gmx.de>
2024-01-17 22:24:43 +0100

    Fixed weird lighting issues with embeddium

--- src/forge/java/team/creative/creativecore/client/render/model/CreativeBakedQuad.java
+++ src/forge/java/team/creative/creativecore/client/render/model/CreativeBakedQuad.java
@@ -10,24 +10,13 @@ public class CreativeBakedQuad extends BakedQuad {
     
     public final RenderBox cube;
     
-    public CreativeBakedQuad(BakedQuad quad, RenderBox cube, int tintedColor, boolean shouldOverrideColor) {
-        this(quad, cube, tintedColor, shouldOverrideColor, quad.getDirection(), false);
+    public CreativeBakedQuad(int[] vertices, BakedQuad quad, RenderBox cube, int tintedColor, boolean shouldOverrideColor) {
+        this(vertices, quad, cube, tintedColor, shouldOverrideColor, quad.getDirection());
     }
     
-    public CreativeBakedQuad(BakedQuad quad, RenderBox cube, int tintedColor, boolean shouldOverrideColor, Direction direction) {
-        this(quad, cube, tintedColor, shouldOverrideColor, direction, false);
-    }
-    
-    private CreativeBakedQuad(BakedQuad quad, RenderBox cube, int tintedColor, boolean shouldOverrideColor, Direction facing, boolean something) {
-        super(copyArray(quad.getVertices()), shouldOverrideColor ? tintedColor : quad.getTintIndex(), facing, quad.getSprite(), quad.isShade());
+    public CreativeBakedQuad(int[] vertices, BakedQuad quad, RenderBox cube, int tintedColor, boolean shouldOverrideColor, Direction facing) {
+        super(vertices, shouldOverrideColor ? tintedColor : quad.getTintIndex(), facing, quad.getSprite(), quad.isShade());
         this.cube = cube;
-    }
-    
-    private static int[] copyArray(int[] array) {
-        int[] newarray = new int[array.length];
-        for (int i = 0; i < array.length; i++)
-            newarray[i] = array[i];
-        return newarray;
     }
     
     public void updateAlpha() {
