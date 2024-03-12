094a9aeed3934ad4f18423388135ba1a3f315878
J-RAP <SrRapero720@hotmail.com>
2024-03-12 17:56:47 -0600

    fixed bad optimized constructor

--- src/main/java/team/creative/creativecore/common/config/group/Usergroup.java
+++ src/main/java/team/creative/creativecore/common/config/group/Usergroup.java
@@ -1,24 +1,24 @@
 package team.creative.creativecore.common.config.group;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import net.minecraft.world.entity.player.Player;
 import team.creative.creativecore.common.config.api.CreativeConfig;
 import team.creative.creativecore.common.util.player.PlayerSelector;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Usergroup {
     
     @CreativeConfig
-    public List<PlayerSelector> filters = new ArrayList<>();
+    public List<PlayerSelector> filters;
     
     public Usergroup() {
-        
+        this.filters = new ArrayList<>();
     }
     
     public Usergroup(PlayerSelector... selectors) {
-        filters.addAll(Arrays.asList(selectors));
+        this.filters = Arrays.asList(selectors);
     }
     
     public boolean is(Player player) {
@@ -27,5 +27,4 @@ public class Usergroup {
                 return false;
         return true;
     }
-    
 }
