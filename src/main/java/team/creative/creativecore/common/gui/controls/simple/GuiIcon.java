6dde8976786a4f214df4add5f2eb4a16c9ce8277
J-RAP <SrRapero720@hotmail.com>
2024-03-21 20:16:09 -0600

    dont rect if shadow is NONE

--- src/main/java/team/creative/creativecore/common/gui/controls/simple/GuiIcon.java
+++ src/main/java/team/creative/creativecore/common/gui/controls/simple/GuiIcon.java
@@ -106,9 +106,11 @@ public class GuiIcon extends GuiControl {
             width = height = size;
         }
 
-        this.shadow.glColor();
-        GuiRenderHelper.textureRect(pose, x + 1, y + 1, width, height, (float) this.icon.minX(), (float) this.icon.minY(), (float) (this.icon
-                .minX() + this.icon.width()), (float) (this.icon.minY() + this.icon.height()));
+        if (this.shadow == Color.NONE) {
+            this.shadow.glColor();
+            GuiRenderHelper.textureRect(pose, x + 1, y + 1, width, height, (float) this.icon.minX(), (float) this.icon.minY(), (float) (this.icon
+                    .minX() + this.icon.width()), (float) (this.icon.minY() + this.icon.height()));2
+        }
 
         this.color.glColor();
         GuiRenderHelper.textureRect(pose, x, y, width, height, (float) this.icon.minX(), (float) this.icon.minY(), (float) (this.icon
