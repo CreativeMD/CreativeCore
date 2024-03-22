7f81de68d042330a47fa6eb00b454421298a0588
J-RAP <SrRapero720@hotmail.com>
2024-03-21 18:08:35 -0600

    use content size instead of control size

--- src/main/java/team/creative/creativecore/common/gui/controls/simple/GuiIcon.java
+++ src/main/java/team/creative/creativecore/common/gui/controls/simple/GuiIcon.java
@@ -97,7 +97,7 @@ public class GuiIcon extends GuiControl {
         RenderSystem.setShader(GameRenderer::getPositionTexShader);
         RenderSystem.setShaderTexture(0, this.icon.location());
 
-        int x = 0, y = 0, width = control.getWidth(), height = control.getHeight();
+        int x = 0, y = 0, width = control.getContentWidth(), height = control.getContentHeight();
         if (squared) {
             int size = Math.min(width, height);
             int diff = Math.abs(width - height);
