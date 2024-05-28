0eb979f4e29097134bcae21c4fa08f4616933729
CreativeMD <Creative-Multimedia-Design@gmx.de>
2024-05-28 19:12:04 +0200

    Fixed errors caused by pull request #233

--- src/fabric/java/team/creative/creativecore/mixin/AbstractContainerScreenMixin.java
+++ src/fabric/java/team/creative/creativecore/mixin/AbstractContainerScreenMixin.java
@@ -1,19 +1,20 @@
 package team.creative.creativecore.mixin;
 
-import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
-import net.minecraft.network.chat.Component;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.network.chat.Component;
+
 @Mixin(AbstractContainerScreen.class)
-public abstract class MouseHandlerMixin extends Screen {
-    protected MouseHandlerMixin(Component title) {
+public abstract class AbstractContainerScreenMixin extends Screen {
+    protected AbstractContainerScreenMixin(Component title) {
         super(title);
     }
-
+    
     @Inject(method = "mouseReleased", at = @At(value = "HEAD"))
     private void handleWhenYouReleaseMouse(double mouseX, double mouseY, int button, CallbackInfoReturnable<Boolean> cir) {
         // PORTED FIX FROM FORGE: this was required to make sliders work properly
