47a0af8df5cb995b582938bd3674f82633987924
CreativeMD <Creative-Multimedia-Design@gmx.de>
2024-09-21 22:32:20 +0200

    Added dist to @Mod annotation (shadowed from NeoForge)

--- src/fabric/java/net/neoforged/fml/common/Mod.java
+++ src/fabric/java/net/neoforged/fml/common/Mod.java
@@ -1,7 +1,11 @@
 package net.neoforged.fml.common;
 
+import net.neoforged.api.distmarker.Dist;
+
 public @interface Mod {
     
     public String value();
+    
+    public Dist[] dist() default { Dist.CLIENT, Dist.DEDICATED_SERVER };
     
 }
