41c2338c473eca666294671e6ea0675cfd24f1fa
CreativeMD <Creative-Multimedia-Design@gmx.de>
2025-03-07 16:34:54 +0100

    Added VertexConsumer parameter for RenderBox and VectorFan

--- src/main/java/team/creative/creativecore/common/util/math/geo/VectorFan.java
+++ src/main/java/team/creative/creativecore/common/util/math/geo/VectorFan.java
@@ -7,12 +7,8 @@ import java.util.function.BiConsumer;
 
 import org.joml.Matrix4f;
 
-import com.mojang.blaze3d.vertex.BufferUploader;
-import com.mojang.blaze3d.vertex.DefaultVertexFormat;
 import com.mojang.blaze3d.vertex.PoseStack.Pose;
-import com.mojang.blaze3d.vertex.Tesselator;
 import com.mojang.blaze3d.vertex.VertexConsumer;
-import com.mojang.blaze3d.vertex.VertexFormat;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
@@ -234,22 +230,19 @@ public class VectorFan {
         return false;
     }
     
-    public void renderPreview(Matrix4f matrix, int red, int green, int blue, int alpha) {
-        var builder = Tesselator.getInstance().begin(VertexFormat.Mode.TRIANGLE_FAN, DefaultVertexFormat.POSITION_COLOR);
+    public void renderPreview(Matrix4f matrix, VertexConsumer consumer, int red, int green, int blue, int alpha) {
         for (int i = 0; i < coords.length; i++) {
             Vec3f vec = coords[i];
-            builder.addVertex(matrix, vec.x, vec.y, vec.z).setColor(red, green, blue, alpha);
+            consumer.addVertex(matrix, vec.x, vec.y, vec.z).setColor(red, green, blue, alpha);
         }
-        BufferUploader.drawWithShader(builder.buildOrThrow());
     }
     
-    public void renderPreview(Matrix4f matrix, float offX, float offY, float offZ, float scaleX, float scaleY, float scaleZ, int red, int green, int blue, int alpha) {
-        var builder = Tesselator.getInstance().begin(VertexFormat.Mode.TRIANGLE_FAN, DefaultVertexFormat.POSITION_COLOR);
+    public void renderPreview(Matrix4f matrix, VertexConsumer consumer, float offX, float offY, float offZ, float scaleX, float scaleY, float scaleZ, int red, int green, int blue,
+            int alpha) {
         for (int i = 0; i < coords.length; i++) {
             Vec3f vec = coords[i];
-            builder.addVertex(matrix, vec.x * scaleX + offX, vec.y * scaleY + offY, vec.z * scaleZ + offZ).setColor(red, green, blue, alpha);
+            consumer.addVertex(matrix, vec.x * scaleX + offX, vec.y * scaleY + offY, vec.z * scaleZ + offZ).setColor(red, green, blue, alpha);
         }
-        BufferUploader.drawWithShader(builder.buildOrThrow());
     }
     
     public void forAllEdges(BiConsumer<Vec3f, Vec3f> consumer) {
