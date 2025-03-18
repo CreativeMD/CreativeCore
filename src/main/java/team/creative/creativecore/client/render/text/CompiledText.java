9f567efc46900935005d5dc89be2631d066c5d32
CreativeMD <Creative-Multimedia-Design@gmx.de>
2025-03-18 11:09:37 +0100

    Added copy method sameDimensions to CompiledText

--- src/main/java/team/creative/creativecore/client/render/text/CompiledText.java
+++ src/main/java/team/creative/creativecore/client/render/text/CompiledText.java
@@ -32,6 +32,10 @@ import team.creative.creativecore.common.util.type.list.SingletonList;
 
 public class CompiledText {
     
+    public static CompiledText createAnySize() {
+        return new CompiledText(Integer.MAX_VALUE, Integer.MAX_VALUE);
+    }
+    
     private static int width(FormattedText text) {
         if (text instanceof AdvancedFormattedText adv)
             return adv.width(AdvancedComponentHelper.SPLITTER.width, Style.EMPTY);
@@ -462,6 +466,17 @@ public class CompiledText {
         return width;
     }
     
+    public CompiledText sameDimensions() {
+        CompiledText copy = new CompiledText(maxWidth, maxHeight);
+        copy.align = align;
+        copy.valign = valign;
+        copy.lineSpacing = lineSpacing;
+        copy.scale = scale;
+        copy.maxWidthScaled = maxWidthScaled;
+        copy.maxHeightScaled = maxHeightScaled;
+        return copy;
+    }
+    
     public CompiledText copy() {
         CompiledText copy = new CompiledText(maxWidth, maxHeight);
         copy.align = align;
@@ -477,10 +492,6 @@ public class CompiledText {
             components.add(component.copy());
         copy.setText(components);
         return copy;
-    }
-    
-    public static CompiledText createAnySize() {
-        return new CompiledText(Integer.MAX_VALUE, Integer.MAX_VALUE);
     }
     
     public boolean contains(String search) {
