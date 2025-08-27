5abfe33ef5738aa8d3e89994517221dc9ebff5d1
CreativeMD <Creative-Multimedia-Design@gmx.de>
2025-08-27 10:32:21 +0200

    Fixed drop in gui not correctly interacting with every inventory Fixed extraction in gui not correctly interacting with every inventory

--- src/main/java/team/creative/creativecore/CreativeCoreGuiRegistry.java
+++ src/main/java/team/creative/creativecore/CreativeCoreGuiRegistry.java
@@ -30,14 +30,7 @@ public class CreativeCoreGuiRegistry {
         Slot slot = c.slot;
         Player player = c.getPlayer();
         if (slot.hasItem() && slot.mayPickup(player)) {
-            ItemStack drop = slot.getItem().copy();
-            if (ctrl)
-                slot.set(ItemStack.EMPTY);
-            else
-                drop.setCount(1);
-            ItemStack newStack = slot.getItem();
-            newStack.shrink(1);
-            
+            ItemStack drop = slot.remove(ctrl ? Math.min(slot.getMaxStackSize(slot.getItem()), slot.getItem().getCount()) : 1);
             player.drop(drop, true);
             slot.onTake(player, drop);
             c.changed();
@@ -86,8 +79,10 @@ public class CreativeCoreGuiRegistry {
             return;
         
         int amount = Math.min(t.getAsInt(), slot.getMaxStackSize(stack) - stack.getCount());
-        ItemStack extract = stack.copy();
-        extract.setCount(amount);
+        ItemStack extract = slot.remove(amount);
+        
+        if (extract.isEmpty())
+            return;
         
         for (IGuiInventory inv : layer.inventoriesToExract()) {
             if (inv == c.inventory())
