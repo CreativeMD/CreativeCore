d841c24c6781c280ac4d75e475538b9f6ce0cc50
CreativeMD <Creative-Multimedia-Design@gmx.de>
2026-02-07 13:52:37 +0100

    Made setLineNormal in VectorFan public

--- src/main/java/team/creative/creativecore/common/util/math/geo/VectorFan.java
+++ src/main/java/team/creative/creativecore/common/util/math/geo/VectorFan.java
@@ -37,7 +37,7 @@ public class VectorFan {
         setLineNormal(normal, first.x, first.y, first.z, second.x, second.y, second.z);
     }
     
-    protected static void setLineNormal(Vec3f normal, float x1, float y1, float z1, float x2, float y2, float z2) {
+    public static void setLineNormal(Vec3f normal, float x1, float y1, float z1, float x2, float y2, float z2) {
         float f = x2 - x1;
         float f1 = y2 - y1;
         float f2 = z2 - z1;
