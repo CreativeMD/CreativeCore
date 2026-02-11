5f5af90b14c0aa1f720773e1db9083141f0b9a8a
CreativeMD <Creative-Multimedia-Design@gmx.de>
2026-02-11 20:30:41 +0100

    Fixed mixin issue on fabric

--- src/main/java/team/creative/creativecore/mixin/ComponentSerializationMixin.java
+++ src/main/java/team/creative/creativecore/mixin/ComponentSerializationMixin.java
@@ -17,9 +17,8 @@ import team.creative.creativecore.common.util.text.content.ContentItemStack;
 public class ComponentSerializationMixin {
     
     @WrapOperation(method = "createCodec(Lcom/mojang/serialization/Codec;)Lcom/mojang/serialization/Codec;", require = 1, at = @At(value = "INVOKE",
-            target = "createLegacyComponentMatcher([Lnet/minecraft/util/StringRepresentable;Ljava/util/function/Function;Ljava/util/function/Function;Ljava/lang/String;)Lcom/mojang/serialization/MapCodec;"))
-    private static <T extends StringRepresentable, E> MapCodec<E> createLegacyComponentMatcherWrapper(T[] types, Function<T, MapCodec<? extends E>> typeToCodec,
-            Function<E, T> contentToType, String fieldName, Operation<MapCodec<E>> operation) {
+            target = "Lnet/minecraft/network/chat/ComponentSerialization;createLegacyComponentMatcher([Lnet/minecraft/util/StringRepresentable;Ljava/util/function/Function;Ljava/util/function/Function;Ljava/lang/String;)Lcom/mojang/serialization/MapCodec;"))
+    private static <T extends StringRepresentable, E> MapCodec<E> createLegacyComponentMatcherWrapper(T[] types, Function<T, MapCodec<? extends E>> typeToCodec, Function<E, T> contentToType, String fieldName, Operation<MapCodec<E>> operation) {
         T[] newTypes = (T[]) new StringRepresentable[types.length + 1];
         for (int i = 0; i < types.length; i++)
             newTypes[i] = types[i];
