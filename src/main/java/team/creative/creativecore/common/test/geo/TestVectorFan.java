7711d478e7cb16d8d38fb9503ac2228f101c2f8c
CreativeMD <Creative-Multimedia-Design@gmx.de>
2026-02-19 16:39:30 +0100

    Removed IntersectionHelper Switched VectorFan cutMinMax to accurate normal plane method (Fixes bugs with connected textures) Switched OBB plane collision to similar method as VectorFan for cutMinMax

--- src/main/java/team/creative/creativecore/common/test/geo/TestVectorFan.java
+++ src/main/java/team/creative/creativecore/common/test/geo/TestVectorFan.java
@@ -7,9 +7,7 @@ import team.creative.creativecore.common.test.CreativeTest;
 import team.creative.creativecore.common.test.CreativeTestException;
 import team.creative.creativecore.common.test.CreativeTestHelper;
 import team.creative.creativecore.common.util.math.base.Axis;
-import team.creative.creativecore.common.util.math.geo.NormalPlaneF;
 import team.creative.creativecore.common.util.math.geo.VectorFan;
-import team.creative.creativecore.common.util.math.vec.Vec2f;
 import team.creative.creativecore.common.util.math.vec.Vec3f;
 
 public class TestVectorFan extends CreativeTest {
@@ -49,26 +47,12 @@ public class TestVectorFan extends CreativeTest {
                 for (int y = 0; y < 4; y++) {
                     for (int x2 = 1; x2 < 4; x2++) {
                         for (int y2 = 1; y2 < 4; y2++) {
-                            var coords = fan.cutMinMax(one, two, axis, x, y, x + x2, y2);
-                            VectorFan toTest;
-                            if (coords == null || coords.length < 3)
-                                toTest = null;
-                            else
-                                toTest = new VectorFan(coords);
-                            
-                            VectorFan other = fan.copy();
-                            NormalPlaneF[] planes = new NormalPlaneF[] { new NormalPlaneF(one, x, one.facing(false)), new NormalPlaneF(one, x + x2, one.facing(
-                                true)), new NormalPlaneF(two, y, two.facing(false)), new NormalPlaneF(two, y + y2, two.facing(true)) };
-                            if (!other.cutWithoutCopy(planes)) {
-                                helper.assertTrue(toTest == null, "Still has result even though it should not %s, original: %s bounds[%s, %s]", toTest, fan, new Vec2f(x, y),
-                                    new Vec2f(x + x2, y + y2));
-                                continue;
-                            }
-                            
-                            helper.assertTrue(toTest != null, "No result provided even there is %s", other);
-                            helper.assertTrue(toTest.equalsIgnoreOrder(other, axis),
-                                "Results must be equal correct: " + other + " incorrect: " + toTest + ", original: %s bounds[%s, %s]", fan, new Vec2f(x, y),
-                                new Vec2f(x + x2, y + y2));
+                            final int minX = x;
+                            final int minY = y;
+                            final int maxX = x + x2;
+                            final int maxY = y + y2;
+                            helper.trackTime("fast", () -> fan.cutMinMax(one, two, axis, minX, minY, maxX, maxY));
+                            helper.incrementCount();
                         }
                     }
                 }
