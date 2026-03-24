dc81078888e6c27ef0f611d2c525d416aa299416
CreativeMD <Creative-Multimedia-Design@gmx.de>
2026-03-24 11:40:22 +0100

    Added register codec to NetworkFieldTypes

---
+++ src/main/java/team/creative/creativecore/common/network/type/NetworkFieldTypeCodec.java
@@ -0,0 +1,30 @@
+package team.creative.creativecore.common.network.type;
+
+import java.lang.reflect.Type;
+
+import org.jetbrains.annotations.Nullable;
+
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.RegistryFriendlyByteBuf;
+import net.minecraft.network.codec.StreamCodec;
+import net.minecraft.network.protocol.PacketFlow;
+
+public class NetworkFieldTypeCodec<T> extends NetworkFieldType<T> {
+    
+    public final StreamCodec<? extends FriendlyByteBuf, T> codec;
+    
+    public NetworkFieldTypeCodec(StreamCodec<? extends FriendlyByteBuf, T> codec) {
+        this.codec = codec;
+    }
+    
+    @Override
+    public void write(T content, Class classType, @Nullable Type genericType, RegistryFriendlyByteBuf buffer, PacketFlow flow) {
+        ((StreamCodec<RegistryFriendlyByteBuf, T>) codec).encode(buffer, content);
+    }
+    
+    @Override
+    public T read(Class classType, @Nullable Type genericType, RegistryFriendlyByteBuf buffer, PacketFlow flow) {
+        return ((StreamCodec<RegistryFriendlyByteBuf, T>) codec).decode(buffer);
+    }
+    
+}
