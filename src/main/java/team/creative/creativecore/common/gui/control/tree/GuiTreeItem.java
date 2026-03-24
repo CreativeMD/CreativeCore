412aa09f11ebb11b7567385b8b93e3660c0a7841
CreativeMD <Creative-Multimedia-Design@gmx.de>
2026-03-24 12:25:43 +0100

    Made GuiTree remember state of checkbox when toggling them

--- src/main/java/team/creative/creativecore/common/gui/control/tree/GuiTreeItem.java
+++ src/main/java/team/creative/creativecore/common/gui/control/tree/GuiTreeItem.java
@@ -135,19 +135,17 @@ public class GuiTreeItem extends GuiParent {
     }
     
     protected void updateControls() {
-        if (tree.hasCheckboxes() == (checkbox != null) && items.isEmpty() == (button == null))
+        if (tree.hasCheckboxes() == (checkbox != null && contains(checkbox)) && items.isEmpty() == (button == null))
             return;
         
         clear();
-        if (items.isEmpty()) {
+        if (items.isEmpty())
             button = null;
-        } else {
+        else
             add(button = (GuiButton) new GuiButtonHoldSlim("expand", x -> toggle()).setTitle(Component.literal("-")));
-        }
+        
         if (tree.hasCheckboxes())
             add(getOrCreateCheckbox());
-        else if (checkbox != null)
-            checkbox = null;
         add(label);
     }
     
