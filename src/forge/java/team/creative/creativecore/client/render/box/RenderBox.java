00591c9ac0722c2045c9bb8750f961dd8736bcac
CreativeMD <Creative-Multimedia-Design@gmx.de>
2026-04-02 13:58:42 +0200

    Fixed some blocks not being rendered when they are colored translucent (blocks from rechiseled for example)

--- src/forge/java/team/creative/creativecore/client/render/box/RenderBox.java
+++ src/forge/java/team/creative/creativecore/client/render/box/RenderBox.java
@@ -391,7 +391,14 @@ public class RenderBox extends AlignedBox {
         if (pos != null)
             rand.setSeed(state.getSeed(pos));
         TODO 1.21.5 YET TO BE IMPLEMENTED
-        List<BakedQuad> blockQuads = blockModel.getQuads(state, facing.toVanilla(), rand, modelData, layer);
+        List<BakedQuad> blockQuads;
+        if (layer == RenderType.TRANSLUCENT && ColorUtils.isTransparent(this.color != -1 ? this.color : defaultColor)) {
+            var set = blockModel.getRenderTypes(state, rand, modelData);
+            blockQuads = new ArrayList<>();
+            for (RenderType type : set)
+                blockQuads.addAll(blockModel.getQuads(state, facing.toVanilla(), rand, modelData, type));
+        } else
+            blockQuads = blockModel.getQuads(state, facing.toVanilla(), rand, modelData, layer);
         
         if (blockQuads.isEmpty())
             return Collections.emptyList();
