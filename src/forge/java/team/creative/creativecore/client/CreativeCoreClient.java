1741c8c5ddce5fc6e9fae996314345cb1c1095d3
CreativeMD <Creative-Multimedia-Design@gmx.de>
2026-04-28 13:31:19 +0200

    Fixed executing reloader manager off thread

--- src/forge/java/team/creative/creativecore/client/CreativeCoreClient.java
+++ src/forge/java/team/creative/creativecore/client/CreativeCoreClient.java
@@ -107,17 +107,19 @@ public class CreativeCoreClient {
         Minecraft minecraft = Minecraft.getInstance();
         ReloadableResourceManager reloadableResourceManager = (ReloadableResourceManager) minecraft.getResourceManager();
         
-        reloadableResourceManager.registerReloadListener(new SimplePreparableReloadListener() {
-            
-            @Override
-            protected Object prepare(ResourceManager p_10796_, ProfilerFiller p_10797_) {
-                return GuiStyle.class; // No idea
-            }
-            
-            @Override
-            protected void apply(Object p_10793_, ResourceManager p_10794_, ProfilerFiller p_10795_) {
-                GuiStyle.reload();
-            }
+        minecraft.execute(() -> {
+            reloadableResourceManager.registerReloadListener(new SimplePreparableReloadListener() {
+                
+                @Override
+                protected Object prepare(ResourceManager p_10796_, ProfilerFiller p_10797_) {
+                    return GuiStyle.class; // No idea
+                }
+                
+                @Override
+                protected void apply(Object p_10793_, ResourceManager p_10794_, ProfilerFiller p_10795_) {
+                    GuiStyle.reload();
+                }
+            });
         });
         
         MenuScreens.register(CreativeCore.GUI_CONTAINER, new ScreenConstructor<ContainerIntegration, ContainerScreenIntegration>() {
